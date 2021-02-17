4065ad888ad48973a61300e1352d1d3ced776bfd
Rendy Artha <rendyartha312@gmail.com>
2021-02-17 10:45:14 +0700

    setup

---
+++ register/index.js
@@ -0,0 +1,7 @@
+const register = () => {
+  const nameInput = document.getElementById('name').value;
+  const emailInput = document.getElementById('email').value;
+  const passwordInput =  document.getElementById('password').value;
+
+  console.log(nameInput, emailInput, passwordInput)
+}
