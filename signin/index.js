5d0f3ad0e81c5d1784dc7a2dfc978cf629304608
Rendy Artha <rendyartha312@gmail.com>
2021-02-17 20:24:05 +0700

    setup register

---
+++ signin/index.js
@@ -0,0 +1,13 @@
+// const signin = () => {
+//   const emailInputSignIn = document.getElementById("email").value;
+//   const passwordInputSignIn = document.getElementById("password").value;
+
+//   const api = "https://6023a95a6bf3e6001766b546.mockapi.io/register"
+//   fetch(api)
+//     .then((response) => response.json())
+//     .then((result) => {
+//       console.log(result)
+//       const user = result.filter(())
+//     })
+//     .catch((error) => console.log(error))
+// }
