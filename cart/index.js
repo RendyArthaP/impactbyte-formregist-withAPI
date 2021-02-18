80764a6fe27476ab1f6536219101faecacfc289e
Rendy Artha <rendyartha312@gmail.com>
2021-02-18 15:56:12 +0700

    register, signin, item per user done

--- cart/index.js
+++ cart/index.js
@@ -1,1 +1,23 @@
-console.log('halo')
+let displayCart = document.querySelector("#cart");
+
+const showCartUser = async() => {
+  let dataUserJSON = localStorage.getItem("user")
+  console.log(dataUserJSON)
+  let dataUserObj = JSON.parse(dataUserJSON)
+  console.log(dataUserObj)
+
+  let displayUser = document.querySelector("#namaUser")
+  displayUser.innerHTML = `<p>Hello, ${dataUserObj.name}</p>`
+
+  const api = `https://6023a95a6bf3e6001766b546.mockapi.io/register/${dataUserObj.id}/cart`
+  const response = await fetch(api)
+  const results = await response.json()
+  console.log(results)
+  results.map((result) => {
+    let displayItem = document.createElement("div")
+    displayItem.innerHTML = `<p>${result.namaBarang}, ${result.price}</p>`
+
+    displayCart.appendChild(displayItem)
+  })
+}
+showCartUser()
--- signin/index.js
+++ signin/index.js
@@ -8,21 +8,36 @@ const signin = () => {
     fetch("https://6023a95a6bf3e6001766b546.mockapi.io/register")
       .then((response) => response.json())
       .then((result) => {
-        const user = result.filter((user) => user.email === emailInputSignIn);
-        console.log(user);
-        console.log(user.length);
-        console.log(user[0]);
-        if(user.length > 0) {
-          alert("User di temukan")
-          if(user[0].password === passwordInputSignIn) {
-            localStorage.setItem("user", JSON.stringify(user[0]))
-            alert("Login Success")
-            window.location.href = "/cart/index.html"
-          } else {
-            alert("Password anda salah") 
-          }
+        // filter
+        // const user = result.filter((user) => user.email === emailInputSignIn);
+
+        // if(user.length > 0) {
+        //   alert("User di temukan")
+        //   if(user[0].password === passwordInputSignIn) {
+        //     localStorage.setItem("user", JSON.stringify(user[0]))
+        //     alert("Login Success")
+        //     window.location.href = "/cart/index.html"
+        //   } else {
+        //     alert("Password anda salah") 
+        //   }
+        // } else {
+        //   alert("User tidak ditemukan")
+        // }
+
+        // find
+        const user = result.find((user) => user.email === emailInputSignIn && user.password === passwordInputSignIn)
+        
+        if(emailInputSignIn === "" || passwordInputSignIn === "") {
+          alert("Please input your data")
         } else {
-          alert("User tidak ditemukan")
+          if(user) {
+            localStorage.setItem("user", JSON.stringify(user));
+            localStorage.setItem("isLoggedin", true);
+            alert("login berhasil");
+            window.location.href = "/cart/index.html";
+          } else {
+            alert("login gagal");
+          }
         }
       })
       .catch((error) => console.log(error));
